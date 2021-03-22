026152957c09c1b1f408dd6de36452678fda8d23
gabrielrih <gabrielrih@gmail.com>
2021-03-22 08:22:31 -0300

    Add files via upload

---
+++ recycle_pool.bat
@@ -0,0 +1,22 @@
+:: Recycling web pool in IIS
+@echo off
+
+GOTO :CHECAPERMISSAO
+GOTO :EXECUTAPROCESSO
+
+:CHECAPERMISSAO
+net session >nul 2>&1
+if %errorLevel% EQU 0 (
+	REM echo everything is fine
+) else (
+	echo ERRO: Run as administrator.
+	echo.
+	pause
+	exit /B
+)
+
+:EXECUTAPROCESSO
+C:
+cd %windir%\System32\inetsrv
+appcmd.exe recycle apppool /apppool.name:poolName
+timeout 5
